0054dd168bb64fcc2e075feedf21ab91d01c0eb3
Daria Matveeva <co-email@yandex.ru>
2023-07-26 18:45:37 +0300

    PW14: payload bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,22 +2,23 @@
 const { Unauthorized } = require('../utils/errors');
 const { checkAuth } = require('../utils/token');
 
-const auth = (req, res, next) => {
-  // console.log(req.cookies);
+const auth = async (req, res, next) => {
+  console.log(req.cookies);
 
   if (!req.cookies) {
     throw new Unauthorized('Доступ отклонен');
   }
-  const token = req.cookies.jwt;
-  // console.log(`Токен ${token}`);
+  const token = await req.cookies.jwt;
+  console.log(`Токен ${token}`);
 
   const payload = checkAuth(token);
-  // console.log(payload);
+  console.log(payload);
 
   if (!payload) {
     throw new Unauthorized('Вы не авторизированы');
   }
   req.user = payload;
+
   next();
 };
 
